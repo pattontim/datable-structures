70557df1950540d9beb34a39f05d815d06f5487a
Tim Patton <timothyp.sci@gmail.com>
2019-06-17 20:51:19 -0400

    Replace object reference with int id

--- Python/main.py
+++ Python/main.py
@@ -2,11 +2,17 @@ import _ctypes
 import gc
 import faulthandler
 
+print("None has id ", id(None))
+
 def di(obj_id):
     return _ctypes.PyObj_FromPtr(obj_id)
 
 class Node(object):
         def __init__(self, data, prev, next):
+            # FIXME special case - to prevent reference=0
+            if(prev == None and next == None):
+                pass
+
             self.data = data
             # self.prev = prev
             # self.next = next
@@ -16,23 +22,25 @@ class Node(object):
             self.nextprev = Node.xor(prev, next)
             # self.nextprev = (next if next is not None else 0)^(prev if prev is not None else 0)
         def xor(a, b):
-            if(a is None):
-                return b
-            elif(b is None):
-                return a
-            elif(b is None and a is None):
-                return None
-            else:
-                return di(id(a)^id(b))
+            return id(a)^id(b)
+            # if(a is None):
+            #     return id(b)
+            # elif(b is None):
+            #     return id(a)
+            # elif(b is None and a is None):
+            #     return None
+            # else:
+            #     return id(a)^id(b)
         def __xor__(self, other):
-            if(self is None):
-                return other
-            elif(other is None):
-                return self
-            elif(self is None and other is None):
-                return None
-            else:
-                return di(id(self)^id(other))
+            pass
+            # if(self is None):
+            #     return id(other)
+            # elif(other is None):
+            #     return id(self)
+            # elif(self is None and other is None):
+            #     return None
+            # else:
+            #     return id(self)^id(other)
 
 class DoubleList(object):
     head = None
@@ -40,17 +48,20 @@ class DoubleList(object):
 
     def append(self, data):
         new_node = Node(data, None, None)
+        print("Created new node with id ", id(new_node), " and value ", data)
         if self.head is None:
             self.head = self.tail = new_node
         else:
             # new_node.prev = self.tail
             # new_node.next = None
+            # # # new_node.nextprev = Node.xor(self.tail, None)
             new_node.nextprev = Node.xor(self.tail, None)
             # # added - self.tail.prev = 
             # self.tail.next = new_node
             tail_prev = Node.xor(self.tail.nextprev, self.tail)
-            tail_next = new_node
-            self.tail.nextprev = Node.xor(tail_prev, new_node)
+            tail_next = id(new_node)
+            print("Linked node ref ", tail_prev, " to ", tail_next)
+            self.tail.nextprev = tail_prev^tail_next
             self.tail = new_node
  
 # Solution: make XOR return the actual object, which is treated by assignment like an object (doesn't copy)
@@ -77,9 +88,14 @@ class DoubleList(object):
     def show(self):
         print("Show list data:")
         current_node = self.head
+        prev_node = None
         while current_node is not None:
+            temp_node = current_node
             print(current_node.data)
-            current_node = current_node.next
+            access_id = id(prev_node)^current_node.nextprev
+            current_node = di(access_id)
+            prev_node = temp_node
+            # current_node = current_node.next
         # while current_node is not None:
         #     print (current_node.prev.data if hasattr(current_node.prev, "data") else None, 
         #     '->', 
